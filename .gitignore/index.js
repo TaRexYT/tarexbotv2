797e9c4b74a1095251078f6bb0adf42f7ba6f5eb
TaRexYT <37890763+TaRexYT@users.noreply.github.com>
2018-03-29 05:58:14 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+car bot = new Discord.Client();
+
+createImageBitmap.on("ready"), function() {
+    bot.user.setGame "i love TaRex, !help");
+    concole.log("le bot a bien étes conecte");
+});
+
+bot.login("NDI4NzQwMjQ3Mjg4MDIxMDAy.DZ3qgA.8P7ih6vz4lkDOFVMQNAwV_opIWs");
