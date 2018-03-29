3b138695701c2108d2c4e3e960bb74aa83af979e
TaRexYT <37890763+TaRexYT@users.noreply.github.com>
2018-03-29 06:19:02 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,9 +2,9 @@ const Discord = require("discord.js");
 
 var bot = new Discord.Client();
 
-createImageBitmap.on("ready"), function() {
+bot.on("ready"), function() {
     bot.user.setGame "i love TaRex, !help");
-    concole.log("le bot a bien étes conecte");
+    console.log("le bot a bien étes conecte");
 });
 
 bot.login("NDI4NzQwMjQ3Mjg4MDIxMDAy.DZ3qgA.8P7ih6vz4lkDOFVMQNAwV_opIWs");
